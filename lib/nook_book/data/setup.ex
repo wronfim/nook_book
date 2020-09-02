0bef7b15adfefd5aff58cb32fdeda329afa1999c
Well Ronfim <wronfim@gmail.com>
2020-09-02 11:45:22 -0400

    Implement generic cache and table behaviour

--- lib/nook_book/data/setup.ex
+++ lib/nook_book/data/setup.ex
@@ -1,7 +1,47 @@
 defmodule NookBook.Data.Setup do
+  @tables [
+    NookBook.Data.GenericCache
+  ]
+
   def setup do
     :mnesia.start()
     create_schema()
+    create_tables()
+  end
+
+  def create_tables do
+    @tables
+    |> Enum.each(&create_table/1)
+  end
+
+  def create_table(module) do
+    if table_exists?(module.table_name()) do
+      {:ok, :already_created}
+    else
+      :mnesia.create_table(
+        module.table_name(),
+        attributes: module.table_fields(),
+        type: module.table_type(),
+        index: module.table_indexes(),
+        disc_copies: nodes()
+      )
+    end
+  end
+
+  def table_exists?(table_name) do
+    :tables
+    |> :mnesia.system_info()
+    |> Enum.member?(table_name)
+  end
+
+  def wait_for_tables do
+    table_names()
+    |> :mnesia.wait_for_tables(10_000)
+  end
+
+  def table_names do
+    @tables
+    |> Enum.map(&apply(&1, :table_name, []))
   end
 
   def create_schema do
